982f2db3b5424aa48b15b709f87222ef48ca1f7d
emmanuelsteven <chizzyjiver@gmail.com>
2026-04-03 14:57:54 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  background-color: yellow;
+`;
+
+function App() {
+  return (
+    <di>
+      <H1>The Wild Oasis</H1>
+    </di>
+  );
+}
+
+export default App;
