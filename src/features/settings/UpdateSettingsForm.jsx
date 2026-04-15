f6acaa2b35617bb2fd1af9885f6c343fcbfe617f
emmanuelsteven <chizzyjiver@gmail.com>
2026-04-15 09:11:36 +0100

    update settings

--- src/features/settings/UpdateSettingsForm.jsx
+++ src/features/settings/UpdateSettingsForm.jsx
@@ -1,21 +1,62 @@
-import Form from '../../ui/Form';
-import FormRow from '../../ui/FormRow';
-import Input from '../../ui/Input';
+import Form from "../../ui/Form";
+import FormRow from "../../ui/FormRow";
+import Input from "../../ui/Input";
+import { useSettings } from "./useSettings";
+import Spinner from "../../ui/Spinner";
+import { useUpdateSetting } from "./useUpdateSetting";
 
 function UpdateSettingsForm() {
+  const {
+    isLoading,
+    settings: {
+      miniBookingLength,
+      MaxBookingLength,
+      GuestPerBooking,
+      breakFastPrice,
+    } = {},
+  } = useSettings();
+  const { isUpdating, updateSetting } = useUpdateSetting();
+  if (isLoading) return <Spinner />;
+  function handleUpdate(e, field) {
+    const { value } = e.target;
+    console.log(value);
+    if (!value) return;
+    updateSetting({ [field]: value });
+  }
   return (
     <Form>
-      <FormRow label='Minimum nights/booking'>
-        <Input type='number' id='min-nights' />
+      <FormRow label="Minimum nights/booking">
+        <Input
+          disabled={isUpdating}
+          type="number"
+          id="min-nights"
+          defaultValue={miniBookingLength}
+          onBlur={(e) => handleUpdate(e, "miniBookingLength")}
+        />
       </FormRow>
-      <FormRow label='Maximum nights/booking'>
-        <Input type='number' id='max-nights' />
+      <FormRow label="Maximum nights/booking">
+        <Input
+          type="number"
+          id="max-nights"
+          defaultValue={MaxBookingLength}
+          onBlur={(e) => handleUpdate(e, "MaxBookingLength")}
+        />
       </FormRow>
-      <FormRow label='Maximum guests/booking'>
-        <Input type='number' id='max-guests' />
+      <FormRow label="Maximum guests/booking">
+        <Input
+          type="number"
+          id="max-guests"
+          defaultValue={GuestPerBooking}
+          onBlur={(e) => handleUpdate(e, "GuestPerBooking")}
+        />
       </FormRow>
-      <FormRow label='Breakfast price'>
-        <Input type='number' id='breakfast-price' />
+      <FormRow label="Breakfast price">
+        <Input
+          type="number"
+          id="breakfast-price"
+          defaultValue={breakFastPrice}
+          onBlur={(e) => handleUpdate(e, "breakFastPrice")}
+        />
       </FormRow>
     </Form>
   );
